d6c3d531f71705bc8c609e4d9894b8c77d9476a1
VikashSingh <cinemasworld600@gmail.com>
2026-04-16 17:21:33 +0530

    project added

---
+++ project/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import HeroSection from '../component/HeroSection'
+import FeaturedSection from '../component/FeaturedSection'
+import TrailersSection from '../component/TrailersSection'
+
+const Home = () => {
+  return (
+    <>
+    <HeroSection/>
+    <FeaturedSection/>
+    <TrailersSection/>
+    </>
+  )
+}
+
+export default Home
