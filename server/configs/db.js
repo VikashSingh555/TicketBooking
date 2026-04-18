b3c4832f63217fe31db9b171b218ecfe5063b439
VikashSingh555 <cinemasworld600@gmail.com>
2026-04-18 16:20:12 +0530

    db fix

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,12 +1,20 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 const connectDB = async () => {
-    try {
-        mongoose.connection.on('connected', ()=> console.log('Database Connected'));
-        await mongoose.connect(`${process.env.MONGODB_URI}/quickshow`)
-    } catch (error) {
-        console.log(error.message);
-    }
-}
+  if (isConnected) {
+    console.log("DB already connected");
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGO_URI);
+    isConnected = db.connections[0].readyState;
+    console.log("Database connected");
+  } catch (error) {
+    console.error("DB connection error:", error);
+  }
+};
 
 export default connectDB;
