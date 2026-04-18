71b2bcbc037447856515e302d9a21c36e9f4fa93
VikashSingh555 <cinemasworld600@gmail.com>
2026-04-18 14:41:12 +0530

    fix vercel inngest route

--- server/server.js
+++ server/server.js
@@ -1,33 +1,42 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import connectDB from './configs/db.js';
-import { clerkMiddleware } from '@clerk/express'
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./configs/db.js";
+import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
-import { inngest, functions } from "./inngest/index.js"
-
-
+import { inngest, functions } from "./inngest/index.js";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-await connectDB()
+// Connect database
+await connectDB();
 
-// Middle
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use(clerkMiddleware());
 
-app.use(express.json())
-app.use(cors())
-app.use(clerkMiddleware())
-
-// API routes
-app.get('/', (req, res)=>{res.send('Server is Live!');})
-
-app.use('/api/inngest', serve({ client: inngest, functions }))
-
-
-
-app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
+// Test route
+app.get("/", (req, res) => {
+  res.send("Server is Live!");
 });
 
+// Inngest route
+app.use(
+  "/api/inngest",
+  serve({
+    client: inngest,
+    functions,
+  })
+);
+
+// Start server only for local
+if (process.env.NODE_ENV !== "production") {
+  app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+  });
+}
+
+// Export for Vercel
 export default app;
