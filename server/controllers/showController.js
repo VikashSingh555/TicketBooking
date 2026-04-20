fce3f22be91a162e12046bfe3bb4cdd559fb4b97
VikashSingh555 <cinemasworld600@gmail.com>
2026-04-20 15:39:06 +0530

    update files

---
+++ server/controllers/showController.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+export const getNowPlayingMovies = async (req, res)=>{
+    try {
+        const { data } = await axios.get('https://api.themoviedb.org/3/movie/now_playing', {
+            headers: {Authorization : `Bearer ${process.env.TMDB_API_KEY}`}
+        })
+    } catch (error) {
+        
+    }
+}
