974954cac5d08e522d81c5eddce2ee561fa72e74
VikashSingh555 <cinemasworld600@gmail.com>
2026-04-23 00:32:42 +0530

    Add files via upload

---
+++ server/controllers/userController.js
@@ -0,0 +1,44 @@
+import { clerkClient } from "@clerk/express";
+import Bookings from "../models/Bookings.js";
+
+
+export const getUserBookings = async (req, res) => {
+    try {
+        const user = req.auth().userId;
+        const bookings = await Bookings.find({user}).populate({
+            path: "show",
+            populate: {path: "movie"}
+        }).sort({createdAt: -1})
+        res.json({success: true, bookings})
+    } catch (error) {
+        console.error(error.message)
+        res.json({success: false, message: error.message});
+    }
+}
+
+export const updateFavorite = async (req, res) => {
+    try {
+        const {movieId} = req.body;
+        const userId = req.auth().userId;
+
+        const user = await clerkClient.users.getUser(userId)
+
+        if(!user.privateMetadata.favorites){
+            user.privateMetadata.favorites = []
+        }
+
+        if(!user.privateMetadata.favorites.includes(movieId)){
+            user.privateMetadata.favorites.push(movieId)
+        }else{
+            user.privateMetadata.favorites = user.privateMetadata.favorites.filter(item => item !== movieId)
+        }
+
+        await clerkClient.users.updateUserMetadata(userId, {privateMetadata: user.privateMetadata})
+
+        res.json({success: true, message: "Favorite movies updated"})
+
+    } catch (error) {
+        console.error(error.message)
+        res.json({success: false, message: error.message});
+    }
+}
