220e354a723bcb3a2674b567cba69c532bf0c39b
VikashSingh555 <cinemasworld600@gmail.com>
2026-04-22 23:58:42 +0530

    Update showRoutes.js

--- server/routes/showRoutes.js
+++ server/routes/showRoutes.js
@@ -1,4 +1,4 @@
-import express from 'express'
+import express from 'express';
 import { addShow, getNowPlayingMovies, getShow, getShows } from '../controllers/showController.js';
 import { protectAdmin } from '../middleware/auth.js';
 
