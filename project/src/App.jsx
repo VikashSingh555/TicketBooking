cdcebfa3e79d1fab00e6fdd0da9d3dd701af28d3
VikashSingh555 <cinemasworld600@gmail.com>
2026-04-23 12:19:47 +0530

    Update App.jsx

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -14,6 +14,8 @@ import Dashboard from './pages/admin/Dashboard'
 import AddShows from './pages/admin/AddShows'
 import ListShows from './pages/admin/ListShows'
 import ListBookings from './pages/admin/ListBookings'
+import { useAppContext } from './context/AppContext'
+import { SignIn } from '@clerk/react'
 
 const App = () => {
   const isAdminRoute = useLocation().pathname.startsWith('/admin')
@@ -28,7 +30,11 @@ const App = () => {
       <Route path='/movies/:id/:date' element={<SeatLayout/>}/>
       <Route path='/mybooking' element={<MyBooking/>}/>
       <Route path='/favourite' element={<Favourite/>}/>
-      <Route path='/admin/*' element={<Layout/>}>
+      <Route path='/admin/*' element={user ? <Layout/> : (
+          <div className='min-h-screen flex justify-center items-center'>
+            <SignIn fallbackRedirectUrl={'/admin'} />
+          </div>
+      )}>
         <Route index element={<Dashboard/>}/>
         <Route path='add-shows' element={<AddShows/>}/>
         <Route path='list-shows' element={<ListShows/>}/>
