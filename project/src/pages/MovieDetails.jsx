ccdbade93ce07b2ddf3e4932cdb9ce155b1d9a8e
VikashSingh555 <cinemasworld600@gmail.com>
2026-04-24 14:00:32 +0530

    Update MovieDetails.jsx

--- project/src/pages/MovieDetails.jsx
+++ project/src/pages/MovieDetails.jsx
@@ -7,6 +7,8 @@ import timeFormat from '../lib/timeFormat'
 import DateSelect from '../component/DateSelect'
 import MovieCard from '../component/MovieCard'
 import Loading from '../component/Loading'
+import { useAppContext } from '../context/AppContext'
+import toast from 'react-hot-toast'
 
 const MovieDetails = () => {
 
@@ -15,25 +17,43 @@ const MovieDetails = () => {
   const {id} = useParams()
   const [show, setShow] = useState(null)
 
+   console.log(show)
+
+  const {shows, axios, getToken, user, fetchFavoriteMovies, favoritesMovies, image_base_url} = useAppContext()
+
   const getShow = async()=>{
-    const show = dummyShowsData.find(show => show._id === id)
-    if(show){
+    try {
+      const {data} = await axios.get(`/api/show/${id}`)
+      if(data.success){
+        setShow(data.show);
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleFavorite = async () => {
+    try {
+      if(!user) return toast.error("Please login to proceed")
       
-      setShow({
-        movie : show,
-        dateTime : dummyDateTimeData
-      })
+        const {data} = await axios.post('/api/user/update-favorite', {movieId: id}, {headers: {Authorization: `Bearer ${await getToken()}`}})
+      if(data.success){
+        await fetchFavoriteMovies()
+        toast.success("Favorite movies updated")
+      }
+    } catch (error) {
+      console.log(error)
     }
   }
 
   useEffect(()=>{
     getShow()
-  },[id])
+  }, [id])
 
   return show ? (
     <div className='px-6 md:px-16 lg:px-40 pt-30 md:pt-50'>
       <div className='flex flex-col md:flex-row gap-8 max-w-6xl mx-auto'>
-        <img src={show.movie.poster_path} alt="" className='max-md:mx-auto rounded-xl h-104 max-w-70 object-cover' />
+        <img src={show?.movie?.poster_path ? image_base_url + show.movie.poster_path : ""} alt="" className='max-md:mx-auto rounded-xl h-104 max-w-70 object-cover' />
 
         <div className='relative flex flex-col gap-3'>
           <BlurCricle top='-100px' left='-100px'/>
@@ -54,8 +74,8 @@ const MovieDetails = () => {
               Watch Trailer
               </button>
             <a href="#dateSelect" className='px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer active:scale-95'>Buy Tickets</a>
-            <button className='bg-gray-700 p-2.5 rounded-full transition cursor-pointer active:scale-95'>
-              <Heart className={`w-5 h-5`}/>
+            <button onClick={handleFavorite} className='bg-gray-700 p-2.5 rounded-full transition cursor-pointer active:scale-95'>
+              <Heart className={`w-5 h-5 ${favoritesMovies.find(movie => movie._id === id) ? 'fill-primary text-primary' : "" }`}/>
             </button>
           </div>
 
@@ -66,9 +86,9 @@ const MovieDetails = () => {
     <p className='text-lg font-medium mt-20'>Your Favourite Cast</p>
     <div className='overflow-x-auto no-scrollbar mt-8 pb-4'>
       <div className='flex items-center gap-4 w-max px-4'>
-        {show.movie.casts.slice(0,12).map((cast , index)=> (
+        {show?.movie?.casts?.slice(0,12).map((cast , index)=> (
           <div key={index} className='flex flex-col items-center text-center'>
-            <img src={cast.profile_path} alt="" className='rounded-full h-20 md:h-20 aspect-square object-cover'/>
+            <img src={image_base_url + cast.profile_path} alt="" className='rounded-full h-20 md:h-20 aspect-square object-cover'/>
             <p className='font-medium text-xs mt-3'>{cast.name}</p>
           </div>
         ))}
@@ -78,8 +98,8 @@ const MovieDetails = () => {
 
         <p className='text-lg font-medium mt-20 mb-8'>You May Also Like</p>
         <div className='flex flex-wrap max-sm:justify-center gap-8'>
-            {dummyShowsData.slice(0,4).map((movie , index)=>(
-              <MovieCard key={index} movie={movie} />
+            {shows.slice(0,4).map((show)=>(
+              <MovieCard movie={show.movie} key={show._id} />
             ))}
         </div>
         <div className='flex justify-center mt-20'>
